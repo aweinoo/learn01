d8d94a21b5802033943bc4d75ff7de695e27ebdf
Aman <aman.bansal@mail.vinove.com>
2026-03-05 12:17:07 +0530

    ContactLIst

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCard from './components/FancyCard';
 import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <FlatCards></FlatCards>
         <ElevatedCards></ElevatedCards>
         <FancyCard></FancyCard>
+        <ContactList></ContactList>
         <ActionCard></ActionCard>
       </ScrollView>
     </SafeAreaView>
---
+++ components/ContactList.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import React from 'react';
+
+export default function ContactList() {
+  const contacts = [
+    {
+      uid: 1,
+      name: 'Hitesh Choudhary',
+      status: 'Just an extra ordinary teacher',
+      imageUrl: 'https://avatars.githubusercontent.com/u/11613311?v=4',
+    },
+    {
+      uid: 2,
+      name: 'Anurag Tiwari',
+      status: 'I 🕶 To Code and Teach!',
+      imageUrl: 'https://avatars.githubusercontent.com/u/94738352?v=4',
+    },
+    {
+      uid: 3,
+      name: 'Sanket Singh',
+      status: 'Making your Gpay smooth',
+      imageUrl: 'https://avatars.githubusercontent.com/u/29747452?v=4',
+    },
+    {
+      uid: 4,
+      name: 'Anirudh Jwala',
+      status: 'Building secure Digital banks',
+      imageUrl: 'https://avatars.githubusercontent.com/u/25549847?v=4',
+    },
+  ];
+  return (
+    <View>
+      <Text style={styles.headingText}>ContactList</Text>
+      <ScrollView style={styles.container} scrollEnabled={false}>
+        {contacts.map(({ uid, name, status, imageUrl }) => (
+          <View key={uid} style={styles.userCard}>
+            <Image source={{ uri: imageUrl }} style={styles.userImage} />
+            <View>
+              <Text style={styles.userName}>{name}</Text>
+              <Text style={styles.userStatus}>{status}</Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+  },
+  userCard: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#8D3DAF',
+    padding: 8,
+    borderRadius: 8,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 60 / 2,
+    marginRight: 14,
+  },
+  userName: { fontWeight: '600', fontSize: 16, color: '#FFF' },
+  userStatus: { color: '#FFF', fontSize: 10 },
+});
